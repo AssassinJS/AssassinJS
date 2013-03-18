20334e428e58410067b6ab338d34c662fd6c7c52
Adithya C <adithya_c@setusoftware.com>
2013-03-18 19:22:44 +0530

    removed headers for requests in proxy, now it works, but need to investigate it

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -2,7 +2,6 @@ var common = require('./common');
 var url = require('url');
 var http = require('http');
 var config = require('../system/config');
-var logger = require('../system/logger');
 
 function forwardRequest(request,response)
 {	
@@ -10,32 +9,32 @@ function forwardRequest(request,response)
 	domain = config.getConfig().domain,
 	domainPort = config.getConfig().domainPort,
 	pathstring = request.url;
-	
+
 	//cached request,response objects
 	var cached_request = request;
 	var cached_response = response;		
-	
+
 	//called on getting response from target server
 	var requestComplete = function(res)
 	{
-		logger.write('STATUS: ' + res.statusCode,'proxy.js');
- 		logger.write('HEADERS: ' + JSON.stringify(res.headers),'proxy.js');
+		console.log('STATUS: ' + res.statusCode);
+ 		console.log('HEADERS: ' + JSON.stringify(res.headers));
   		//res.setEncoding('utf8');
   		//res.setEncoding('binary');
   		
   		cached_response.writeHead(res.statusCode,res.headers);
   		
  		res.on('data', function (chunk) { 			
-    		logger.write('BODY: ' + chunk,'proxy.js');
+    		console.log('BODY: ' + chunk);
     		cached_response.write(chunk);	     	 
   		});
   		
   		res.on('end',function() {  			  			  			    		
-    		cached_response.addTrailers(res.headers);
+    		//cached_response.addTrailers(res.headers);
     		cached_response.end(); 	
   		});
 	};
-			
+
 	//for making a http head request
 	var makeHEADrequest = function(request)
 	{
@@ -43,32 +42,32 @@ function forwardRequest(request,response)
   						host: domain,
  						port: domainPort,
   						path: pathstring,
-  						headers: request.headers,
+  						//headers: request.headers,
   						method: 'HEAD'
 					  };
 
 		var new_request = http.request(options,function(res){
-						
-		logger.write('STATUS: ' + res.statusCode,'proxy.js');
- 		logger.write('HEADERS: ' + JSON.stringify(res.headers),'proxy.js');
+
+		console.log('STATUS: ' + res.statusCode);
+ 		console.log('HEADERS: ' + JSON.stringify(res.headers));
   		
-  		cached_response.writeHead(res.statusCode,res.headers);
+  		cached_response.writeHead(res.statusCode,{});
   		cached_response.end(); 	
   											
 		});
-		
+
 		new_request.on('error', function(e) {
-  			logger.write('problem with request: ' + e.message,'proxy.js');
+  			console.log('problem with request: ' + e.message);
   			cached_response.writeHead(500,{'Content-Type': 'text/plain'});
     		cached_response.write('Invalid URL Specified');
     		cached_response.end();
 		});
-		
+
 		new_request.end();	
-		
+
 		return;
 	};
-	
+
 	//for making a http get request
 	var makeGETrequest = function(request)
 	{
@@ -76,20 +75,20 @@ function forwardRequest(request,response)
   						host: domain,
  						port: domainPort,
   						path: pathstring,
-  						headers: request.headers
+  						//headers: request.headers
 					  };
 
 		http.get(options, requestComplete)
 		.on('error', function(e) {
-  			logger.write('problem with request: ' + e.message,'proxy.js');
+  			console.log('problem with request: ' + e.message);
   			cached_response.writeHead(500,{'Content-Type': 'text/plain'});
     		cached_response.write('Invalid URL Specified');
     		cached_response.end();
 		});
-		
+
 		return;
 	};
-	
+
 	//for making other http requests
 	var makeRequest = function(data)
 	{
@@ -97,28 +96,28 @@ function forwardRequest(request,response)
  						host: domain,
  						port: domainPort,
   						path: pathstring,
-  						headers: cached_request.headers,
+  						//headers: cached_request.headers,
   						method: cached_request.method
 					 };
-					  
+
 		var new_request = http.request(options, requestComplete)
 		.on('error', function(e) {
-  			logger.write('problem with request: ' + e.message,'proxy.js');
+  			console.log('problem with request: ' + e.message);
   			cached_response.writeHead(500,{'Content-Type': 'text/plain'});
     		cached_response.write('Invalid URL Specified');
     		cached_response.end();
 		});
-			
+
 		new_request.write(data);			
 		new_request.end();
 	};
-			
+
 	//for receiving request from source
 	var receiveRequest = function(request)
 	{			
 		//caching Post Content Chunks
 		var fullData = '';
-		
+
 		request.on('data', function(chunk) {		
       		// append the current chunk of data to the content cache
      		fullData += chunk;
@@ -128,10 +127,10 @@ function forwardRequest(request,response)
    			makeRequest(fullData);
   		});
 	};
-	
+
 	//actual execution part starts from here
-	logger.write('Call To '+domain+pathstring,'proxy.js');
-	
+	console.log(domain+pathstring);
+
 	switch(request.method)
 	{
 		case 'HEAD':		
@@ -147,9 +146,9 @@ function forwardRequest(request,response)
 		case 'OPTIONS':												
 						receiveRequest(request);
 						return;	
-						
+
 	}
-		
+
 }
 
 exports.forwardRequest = forwardRequest;

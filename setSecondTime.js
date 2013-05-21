391f293e32104ea598e01503e850b7d0a60af8d3
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2013-05-21 07:34:25 +0530

    type correction for firsttime in config.json

--- setSecondTime.js
+++ setSecondTime.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var config = require('./config/config.json');
-config.firsttime = false;
+config.firsttime = "false";
 fs.writeFileSync('./config/config.json',JSON.stringify(config));

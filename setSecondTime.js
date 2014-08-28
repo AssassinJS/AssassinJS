ab2c667e14a94abb6a4e727fe8190f961635f862
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2014-08-28 19:04:31 +0530

    gh-pages

--- setSecondTime.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var config = require('./config/config.json');
-config.firsttime = "false";
-fs.writeFileSync('./config/config.json',JSON.stringify(config));
--- setUseDB.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var config = require('./config/config.json');
-config.useDB = "true";
-fs.writeFileSync('./config/config.json',JSON.stringify(config));
